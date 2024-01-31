0ada0a866867a74c928e79d09db5f5dc21de9b23
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-01-31 15:10:04 +0700

    change to graphql

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -11,33 +11,68 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   ApiService apiService = ApiService();
+  List<ProductElement> products = []; // Initialize with an empty list
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<ProductElement>>(
-        future: apiService.getProducts(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // If the Future is still running, show a loading indicator
-            return CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            // If the Future completed with an error, display the error message
-            return Text('Error: ${snapshot.error}');
-          } else {
-            // If the Future completed successfully, display the data
-            List<ProductElement> products = snapshot.data ?? [];
-            return ListView.builder(
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(products[index].title),
-                  // Add other widgets to display other information about the product
-                );
-              },
-            );
-          }
-        },
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: FutureBuilder<List<ProductElement>>(
+          future: apiService.getProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // If the Future is still running, show a loading indicator
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              // If the Future completed with an error, display the error message
+              return Text('Error: ${snapshot.error}');
+            } else {
+              // If the Future completed successfully, display the data
+              List<ProductElement> products = snapshot.data ?? [];
+              return ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  // return Text(products[index].title);
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Center(
+                      child: Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            ListTile(
+                              leading: const Icon(Icons.album),
+                              title: Text(products[index].title),
+                              subtitle: Text(products[index].description),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                TextButton(
+                                  child: const Text('BUY TICKETS'),
+                                  onPressed: () {/* ... */},
+                                ),
+                                const SizedBox(width: 8),
+                                TextButton(
+                                  child: const Text('LISTEN'),
+                                  onPressed: () {/* ... */},
+                                ),
+                                const SizedBox(width: 8),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
