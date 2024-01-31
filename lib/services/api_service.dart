8c15facb89d66782666d50970bffd8e2716b8273
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-01-31 14:22:31 +0700

    change to graphql

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,19 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_assigment07/models/product_model.dart';
 import 'package:flutter_assigment07/utils/logger.dart';
 
 class ApiService {
   final dio = Dio();
 
-  getProducts() async {
+  Future<List<ProductElement>> getProducts() async {
     try {
       final result = await dio.get('https://dummyjson.com/products');
       logger.f(result.data);
+      final Product products = productFromJson(result.data);
+      return products.products;
     } catch (err) {
       logger.e(err);
+      return [];
     }
   }
 }
