8a3df2b478a3e533e876911ca884926e94bcb369
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-01-31 14:14:41 +0700

    change to graphql

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_assigment07/services/api_service.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
   const ProductScreen({super.key});
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ApiService apiService = ApiService();
+    apiService.getProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,14 +1,15 @@
 import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
+import 'package:flutter_assigment07/utils/logger.dart';
 
 class ApiService {
   final dio = Dio();
 
   getProducts() async {
-    try{
-
-    }catch(err){
-      
+    try {
+      final result = await dio.get('https://dummyjson.com/products');
+      logger.f(result.data);
+    } catch (err) {
+      logger.e(err);
     }
   }
 }
