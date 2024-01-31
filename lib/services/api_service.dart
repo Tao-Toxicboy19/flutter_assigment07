6f1683f34b9bd029d5b45fd6adbca612e0e1c22c
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-01-31 14:07:29 +0700

    change to graphql

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,14 @@
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 class ApiService {
   final dio = Dio();
 
+  getProducts() async {
+    try{
+
+    }catch(err){
+      
+    }
+  }
 }
